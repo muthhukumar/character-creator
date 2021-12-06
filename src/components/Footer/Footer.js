6f7ec5285804a99395cb1d20a3a277fea124fd37
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2021-12-06 14:03:38 +0530

    feat: finished up the workshoop

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      All character assets taken from{' '}
+      <a
+        href="https://www.openpeeps.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.characterCredit}
+      >
+        Open Peeps
+      </a>
+      , by Pablo Stanley
+    </footer>
+  )
+}
+
+export default Footer
